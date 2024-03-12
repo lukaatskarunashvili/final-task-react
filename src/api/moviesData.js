9d5300bbc46c80c7d7363dff3c3e114520bd7085
lukatskarunashvili <lukaawkarunashvili0@gmail.com>
2024-03-12 20:22:11 +0400

    .

--- src/api/moviesData.js
+++ src/api/moviesData.js
@@ -2,8 +2,8 @@ export async function MoviesData() {
   const resp = await fetch("https://imdb-top-100-movies.p.rapidapi.com/", {
     method: "GET",
     headers: {
-        'X-RapidAPI-Key': '331a6d0136mshbec8538ac87234ap11baf7jsn0744d1f0fc54',
-        'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+      'X-RapidAPI-Key': 'd75ca3b557msh77cbb2ee688493cp182509jsnc99fc2e287c4',
+      'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
     },
   });
   if (resp.ok) {
